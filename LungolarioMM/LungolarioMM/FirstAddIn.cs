e8723d7603c9c5a3b543e160abec85306a19aed5
Muhammad Zohaib Zahid <zohaibtxt@gmail.com>
2017-05-09 04:43:25 +0500

    V0.1
    First commit with a test function of Hello World

---
+++ LungolarioMM/LungolarioMM/FirstAddIn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelDna.Integration;
+
+namespace LungolarioMM
+{
+    public class FirstAddIn
+    {
+
+        [ExcelFunction(Description = "Adds Hello World to the start of the string given as input")]
+        public static string HelloWorld(string name)
+        {
+            return "Hello World " + name;
+        }
+
+    }
+}
