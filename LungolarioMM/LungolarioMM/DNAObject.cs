3a7bf4e98b4ac29f32a04979c153a14116e8c8cf
Muhammad Zohaib Zahid <zohaibtxt@gmail.com>
2017-05-11 05:36:33 +0200

    Reworked classes for abstraction. Reworked Object Creation Method.

---
+++ LungolarioMM/LungolarioMM/DNAObject.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LungolarioMM
+{
+    public abstract class DNAObject
+    {
+        public string name;
+        public virtual void CreateObject(string name)
+        {
+            this.name = name;
+        }
+    }
+    public class Model : DNAObject
+    {
+        public override void CreateObject(string name)
+        {
+            base.CreateObject(name);
+        }
+    }
+    public class Curve : DNAObject
+    {
+        public override void CreateObject(string name)
+        {
+            base.CreateObject(name);
+        }
+    }
+    public class Vol : DNAObject
+    {
+        public override void CreateObject(string name)
+        {
+            base.CreateObject(name);
+        }
+    }
+    public class Results : DNAObject
+    {
+        public override void CreateObject(string name)
+        {
+            base.CreateObject(name);
+        }
+    }
+    public class MMObjectHandler
+    {
+        public List<DNAObject> objs = new List<DNAObject>();
+        public void CreateObject(string name,string type)
+        {
+            switch(type)
+            {
+                case "MODEL":
+                    DNAObject model = new Model();
+                    model.name = name;
+                    objs.Add(model);
+                    break;
+                case "CURVE":
+                    DNAObject curve = new Curve();
+                    curve.name = name;
+                    objs.Add(curve);
+                    break;
+                case "VOL":
+                    DNAObject vol = new Vol();
+                    vol.name = name;
+                    objs.Add(vol);
+                    break;
+                case "RESULTS":
+                    DNAObject results = new Results();
+                    results.name = name;
+                    objs.Add(results);
+                    break;
+                default:
+                    throw new Exception("Cannot add this object. No type found!(" + type +")");
+            }
+            
+        }
+        public int CountObjectByName(string name)
+        {
+            return objs.Count(x => x.name == name);
+        }
+        public string CountAllObjects()
+        {
+            string countedObjs="";
+            var q = from x in objs
+                    group x by x.name into g
+                    let count = g.Count()
+                    orderby count descending
+                    select new { Value = g.Key, Count = g.Count() };
+            foreach (var x in q)
+            {
+                countedObjs += x.Value + ":" + x.Count +" ";
+            }
+            return countedObjs;
+        }
+    }
+}
--- LungolarioMM/LungolarioMM/Lungolario.cs
+++ LungolarioMM/LungolarioMM/Lungolario.cs
@@ -6,71 +6,32 @@ using ExcelDna.Integration;
 
 namespace LungolarioMM
 {
-    static class CreateObj
-    {
-        public enum ObjType
-        {
-            MODEL,
-            VOL,
-            CURVE,
-            RESULTS
-        };
-        public static List<string> Objs = new List<string>();
-        static int ReturnCount(string name, ObjType type)
-        {
-            switch(type)
-            {
-                case ObjType.MODEL:
-                    return CountNumberOfOccurences(name, ObjType.MODEL);
-                case ObjType.VOL:
-                    return CountNumberOfOccurences(name, ObjType.VOL);
-                case ObjType.CURVE:
-                    return CountNumberOfOccurences(name, ObjType.CURVE);
-                case ObjType.RESULTS:
-                    return CountNumberOfOccurences(name, ObjType.RESULTS);
-                default:
-                    return -1;
-            }
-        }
-        public static int CountNumberOfOccurences(string name, ObjType type)
-        {
-            int counter = 0;
-            foreach (string obj in Objs)
-            {
-                if (obj.ToString() == ConcatenateNameAndType(name, type.ToString()))
-                    counter++;
-            }
-            AddObj(name,type.ToString());
-            return counter;
-        }
-        static void AddObj(string name, string type)
-        {
-            Objs.Add(ConcatenateNameAndType(name , type));
-        }
-        static string ConcatenateNameAndType(string name, string type)
-        {
-            return name + "," + type;
-        }
-    }
     public class Lungolario
     {
+        static MMObjectHandler mmObjHandler= new MMObjectHandler();
+        Lungolario()
+        {
+            mmObjHandler = new MMObjectHandler();
+        }
         [ExcelFunction(Description = "Adds Hello World to the start of the string given as input")]
         public static string HelloWorld(string name)
         {
             return "Hello World " + name;
         }
+        [ExcelFunction(Description = "Creates an object with name and type")]
         public static string mmCreateObj(string objName, string objType, object[,] range)
         {
-            if (objType == "MODEL")
-                return objName + ":" + CreateObj.CountNumberOfOccurences(objName, CreateObj.ObjType.MODEL);
-            else if (objType == "VOL")
-                return objName + ":" + CreateObj.CountNumberOfOccurences(objName, CreateObj.ObjType.VOL);
-            else if (objType == "CURVE")
-                return objName + ":" + CreateObj.CountNumberOfOccurences(objName, CreateObj.ObjType.CURVE);
-            else if (objType == "RESULTS")
-                return objName + ":" + CreateObj.CountNumberOfOccurences(objName, CreateObj.ObjType.RESULTS);
-            else
-                return "Error. No name/type found.";
+            string anyexception="";
+            ExcelReference caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
+            try
+            {
+                mmObjHandler.CreateObject(objName, objType.ToUpper());
+            }
+            catch(Exception e)
+            {
+                anyexception = e.Message.ToString();
+            }
+            return mmObjHandler.CountAllObjects() + anyexception;
         }
     }
 }
